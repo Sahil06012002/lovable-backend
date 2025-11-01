c647d2ca792d02da12d5d0d13a5fed74499f3f37
Tushar Gagal <tushargagal1209@gmail.com>
2025-11-01 23:20:12 +0530

    imporved file structure

--- src/server.ts
+++ src/server.ts
@@ -1,52 +1,6 @@
-import express from "express"
-import dotenv from "dotenv"
+import { app } from "./app";
+import { PORT } from "./config/env";
 
-dotenv.config()
-
-import {openai} from "@ai-sdk/openai"
-import {streamText, type ModelMessage} from "ai"
-
-const app = express()
-
-app.use(express.json())
-
-app.get("/",(req,res)=>{
-    return res.json({
-        "health" : "runnig"
-    })
-})
-
-
-
-
-app.post("/chat",async (req,res)=>{
-    const reqBody = req.body;
-
-    const prompt = "set up a react js project using vite"
-
-
-    const userMessage : Array<ModelMessage> = [{ role : 'user', content  : prompt}]
-
-    const result = streamText({
-    model: openai('gpt-4o'),
-    messages : userMessage,
-  });
-
-  const content = await result.content
-  console.log(content)
-  let finalOutput = ""
-
-  for await (const delta of result.textStream) {
-finalOutput += delta
-  }
-  console.log(finalOutput)
-
-    return res.json({
-        "answer" : finalOutput
-    })
-})
-
-
-app.listen(3000,()=>{
-    console.log("server running")
-})
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
